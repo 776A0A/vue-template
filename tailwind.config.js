396875f7b68a9580989c9d54615583c5ac90a1d5
王捷 <har3dy@icloud.com>
2023-06-07 17:46:35 +0800

    feat: 添加tailwind插件

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,46 +2,50 @@ import plugin from 'tailwindcss/plugin'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
-    theme: { extend: {} },
-    plugins: [
-        plugin(({ addComponents, addUtilities }) => {
-            addUtilities({
-                '.content-visibility-auto': { 'content-visibility': 'auto' },
-            })
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
+  theme: { extend: {} },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+    plugin(({ addComponents, addUtilities }) => {
+      addUtilities({
+        '.content-visibility-auto': { 'content-visibility': 'auto' },
+      })
 
-            addComponents({
-                '.center-flex': {
-                    display: 'flex',
-                    'justify-content': 'center',
-                    'align-items': 'center',
-                },
-                '.center-abs': {
-                    position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate3d(-50%, -50%, 0)',
-                },
-                '.center-x-abs': {
-                    position: 'absolute',
-                    left: '50%',
-                    transform: 'translate3d(-50%, 0, 0)',
-                },
-                '.center-y-abs': {
-                    position: 'absolute',
-                    top: '50%',
-                    transform: 'translate3d(0, -50%, 0)',
-                },
-                '.full': {
-                    width: '100%',
-                    height: '100%',
-                },
-                '.fullscreen': {
-                    width: '100%',
-                    height: '100%',
-                    display: 'fixed',
-                },
-            })
-        }),
-    ],
+      addComponents({
+        '.center-flex': {
+          display: 'flex',
+          'justify-content': 'center',
+          'align-items': 'center',
+        },
+        '.center-abs': {
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate3d(-50%, -50%, 0)',
+        },
+        '.center-x-abs': {
+          position: 'absolute',
+          left: '50%',
+          transform: 'translate3d(-50%, 0, 0)',
+        },
+        '.center-y-abs': {
+          position: 'absolute',
+          top: '50%',
+          transform: 'translate3d(0, -50%, 0)',
+        },
+        '.full': {
+          width: '100%',
+          height: '100%',
+        },
+        '.fullscreen': {
+          width: '100%',
+          height: '100%',
+          display: 'fixed',
+        },
+      })
+    }),
+  ],
 }
